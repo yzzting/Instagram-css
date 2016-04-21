7ed47b72d963f98296648aa70d848e01509ca146
yzzting <yzz1995@aliyun.com>
2016-04-21 23:15:28 +0800

    add filter option

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ gulp.task('serve', function() {
 
 gulp.task('watch', ['less', 'jade'], function() {
     gulp.watch(['source/less/*.less'], ['less'])
+    gulp.watch(['source/less/Filter/*.less'],['less'])
     gulp.watch(['templates/jade/index.jade'], ['jade'])
 })
 gulp.task('build', ['jade', 'less']);
