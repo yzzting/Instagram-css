5cf7c218568fb4a4c57b2ecca04fdda69946f1b2
yzzting <yzz1995@aliyun.com>
2016-05-04 16:58:19 +0800

    add onload file

--- dist/script/img.js
+++ dist/script/img.js
@@ -12,11 +12,30 @@ function updateSrc(src) {
     }
 }
 
-document.addEventListener('click',function(event){
+function fileSelect(e) {
+    e = e || window.event;
+    var files = this.files;
+    var p = document.getElementById('preview');
+
+    for (var i = 0, f; f = files[i]; i++) {
+        var reader = new FileReader();
+        reader.onload = (function(file) {
+            return function(e) {
+                console.log(this.result);
+                updateSrc(this.result);
+            };
+        })(f);
+        //读取文件内容
+        reader.readAsDataURL(f);
+    }
+}
+document.getElementById('filename').addEventListener('change', fileSelect, false);
+
+document.addEventListener('click', function(event) {
     if (/demoImg/.test(event.target.className)) {
         pickSample(event.target)
     }
-},false)
+}, false)
 inputField.addEventListener("input", function() {
     updateSrc(this.value)
 }, false)
